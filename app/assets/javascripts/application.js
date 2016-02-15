78aa1523c69ff9e51590ac5bef58cc7d5b3a16c4
Rohit <roohit1234@gmail.com>
2016-02-15 13:33:51 +0530

    Navbar for singed in users nad signout users

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,18 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
-//= require bootstrap
 //= require jquery_ujs
+//= require bootstrap/affix
+//= require bootstrap/alert
+//= require bootstrap/button
+//= require bootstrap/carousel
+//= require bootstrap/collapse
+//= require bootstrap/dropdown
+//= require bootstrap/modal
+//= require bootstrap/scrollspy
+//= require bootstrap/tab
+//= require bootstrap/transition
+//= require bootstrap/tooltip
+//= require bootstrap/popover
 //= require turbolinks
 //= require_tree .
